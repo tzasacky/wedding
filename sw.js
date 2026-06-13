df747b80a0e69c1b0ae7cea99a1f1088f0f08d40
Tye Zasacky <tzasacky@gmail.com>
2026-06-12 20:07:26 -0400

    Changed charity, small text updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'wedding-1b0830de';
+const CACHE_VERSION = 'wedding-9c46d89d';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const IMAGE_CACHE = `${CACHE_VERSION}-images`;
 
